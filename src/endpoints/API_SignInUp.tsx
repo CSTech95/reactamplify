a960e92e85635eb9a83a9079a16cbe5a44751a87
KyungminUm <snownaul2@gmail.com>
2022-11-16 14:50:02 -0500

    SE-67 endpoint folder created

---
+++ src/endpoints/API_SignInUp.tsx
@@ -0,0 +1,9 @@
+
+import { DataStore } from "aws-amplify";
+
+//	async function addVehicle() {
+//		await DataStore.save(new Cars({ make, model, isComplete: false }));
+//		setModalVisible(false);
+//		setMake("");
+//		setModel("");
+//	}
